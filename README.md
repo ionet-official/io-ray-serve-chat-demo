7c02097d8268737d59e0bd7f583b7d3620203cd0
Aline Almeida <aline@io.net>
2024-11-30 20:14:33 +0400

    Update ray serve HF model chat demo

--- README.md
+++ README.md
@@ -1,25 +1,63 @@
 # io-ray-serve-chat-demo
+
 a Ray Serve Chat Demo Serving Hugging Face Models
 
+## How to get started
 
-# How to get started 
-
-1. Open Up Io.net Account
+1. Open Up [io.net](https://io.net) account
 2. Follow through standard procedure on launching a Ray Cluster. Select a small cluster, for example 4 T4.
-3. When the cluster is ready, select Visual Studio Code
-4. Launch Visual studio code terminal and clone this repo `git clone https://github.com/ionet-official/io-ray-serve-chat-demo.git`
-5. Go to the folder `cd  io-ray-serve-chat-demo`
-6. Start the chat server via `ray serve run chat.yaml`
-7. Wait till the Ray serve deploys the chat app across workers.
-8. Remember the current vscode url. It looks like this  `https://vscode-1d47a.tunnels.io.systems/`
-9. Replace the `vscode` section with `exposed-service`, you will obtain the Chat server serve endpoint
-10. You can use below code snippet to interact with the Ray serve application created
+3. When the cluster is ready, select Visual Studio Code (VSCode)
+   ![IO Cloud Ray Cluster T4](images/io-cloud-cluster-T4.png)
+
+4. Launch Visual studio code terminal and clone this repo
+
+```bash
+git clone https://github.com/ionet-official/io-ray-serve-chat-demo.git
 ```
+
+5. Go to the folder
+
+```bash
+cd  io-ray-serve-chat-demo
+```
+
+6. Start the chat server via
+
+```bash
+serve run chat.yaml
+```
+
+   ![Serve run chat](images/serve-run-chat-example.png)
+
+7. Wait till the Ray serve deploys the chat app across workers. You will see on the terminal a "Model loaded" message.
+8. Test your Chatbot from the cluster. Open a new terminal and run the sample chat client
+
+```bash
+python chat_client.py
+```
+
+   ![chat client example](images/chat-client-example.png)
+
+9. Test your Chatbot server endpoint from outside the Cluster
+   1. Server endpoint: `https://exposed-service-[YOUR-CLUSTER-SUFFIX].tunnels.io.systems/`
+   2. If your cluster suffix is `1d47a`, then: `https://exposed-service-1d47a.tunnels.io.systems/`
+   3. One way to identify your prefix is from the the VSCode URL, which looks like `https://vscode-1d47a.tunnels.io.systems/`
+   4. You can use below code snippet to interact with the Ray serve application created (update the endpoint to your server)
+
+```python
 import requests
-import json
-message = 'I love carbs myself -- bread is tasty.'
+SERVER_ENDPOINT = "https://exposed-service-1d47a.tunnels.io.systems/"
+message = "What is the capital of France?"
 history = []
-json_doc = json.dumps({ 'user_input' : message, "history": []})
-result = requests.post('https://exposed-service-1d47a.tunnels.io.systems/', json = json_doc).json()
-print(result)
+response = requests.post(SERVER_ENDPOINT, json={"user_input": message, "history": history})
+print(response.json())
+```
+
+or on a terminal:
+
+```bash
+curl -X POST https://exposed-service-1d47a.tunnels.io.systems/ \
+-H "Content-Type: application/json" \
+-d '{"user_input": "What is the capital of France?", "history": []}'
+
 ```
--- chat.py
+++ chat.py
@@ -1,37 +1,49 @@
-import ray
-import requests, json
-from starlette.requests import Request
+import torch
 from typing import Dict
+
 from ray.serve import Application
 from ray import serve
+from starlette.requests import Request
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-@serve.deployment(ray_actor_options={'num_gpus': 0.5})
+
+
+@serve.deployment(ray_actor_options={"num_gpus": 0.5})
 class Chat:
     def __init__(self, model: str):
         # configure stateful elements of our service such as loading a model
         self._tokenizer = AutoTokenizer.from_pretrained(model)
-        self._model =  AutoModelForSeq2SeqLM.from_pretrained(model).to(0)
+        print(f"Loading model: {model}")
+        self._model = AutoModelForSeq2SeqLM.from_pretrained(model, torch_dtype=torch.float16).to(0)
+        self._max_length = self._model.config.max_position_embeddings
+        print(f"Model loaded. Max length for the model: {self._max_length}")
 
     async def __call__(self, request: Request) -> Dict:
         # path to handle HTTP requests
         data = await request.json()
-        data = json.loads(data)
+
         # after decoding the payload, we delegate to get_response for logic
-        return {'response': self.get_response(data['user_input'], data['history']) }
+        return {"response": self.get_response(data["user_input"], data["history"])}
 
     def get_response(self, user_input: str, history: list[str]) -> str:
         # this method receives calls directly (from Python) or from __call__ (from HTTP)
-        # the history is client-side state and will be a list of raw strings;
-        # for the default config of the model and tokenizer, history should be joined with '</s><s>'
-        inputs = self._tokenizer('</s><s>'.join(user_input), return_tensors='pt').to(0)
-        print(inputs)
-        reply_ids = self._model.generate(**inputs, max_new_tokens=500)
-        print(reply_ids)
-        response = self._tokenizer.batch_decode(reply_ids.cpu(), skip_special_tokens=True)
-        print(response)
-        return response[0]
+        # the history is client-side state and will be a list of raw strings
+        # older pair of messages from history is used as long as it fits the model's max length
 
-def app_builder(args: Dict[str, str])->Application:
-    return Chat.bind(model='facebook/blenderbot-400M-distill')
+        # Trim the history until total input fits max_lenght
+        while True:
+            input_text = "\n".join(history + [user_input])
+            if len(history) == 0 or len(self._tokenizer.encode(input_text)) <= self._max_length:
+                break
+            history = history[2:]
+
+        inputs = self._tokenizer([input_text], max_length=self._max_length, truncation=True, return_tensors="pt").to(0)
+        reply_ids = self._model.generate(**inputs, max_length=self._max_length)
+        response = self._tokenizer.batch_decode(reply_ids, skip_special_tokens=True)
+        return response[0].strip()
+
+
+def app_builder(args: Dict[str, str]) -> Application:
+    return Chat.bind(model="facebook/blenderbot-400M-distill")
+
 
 app = app_builder(None)
