12aa0916cad4adb9299a137400d271afda33f6cc
Aline Almeida <aline@io.net>
2024-12-09 13:10:18 +0100

    Update ray serve HF model chat demo (#1)
    Update ray serve HF model chat demo
    
    - Truncate history to fit under max length handled by the model to avoid errors when the number of interactions increases
    - Fix response handling and interaction example
    - Update instructions

--- chat_client.py
+++ chat_client.py
@@ -1,11 +1,29 @@
 import requests
-import json
-message = 'I love carbs myself -- bread is tasty.'
+
+SERVER_ENDPOINT = "http://localhost:8778/"
+
+def send_message(message, history):
+    try:
+        response = requests.post(SERVER_ENDPOINT, json={"user_input": message, "history": history})
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        print(f"An error occurred: {e}")
+        return None
+
+
 history = []
-json_doc = json.dumps({ 'user_input' : message, "history": []})
-result = requests.post('http://localhost:8888/', json = json_doc).json()
-print(result)
-history += [message, result["response"]]
-json_doc = json.dumps({ 'user_input' : message, 'history' : history })
-result = requests.post('http://localhost:8888/', json = json_doc).json()
-print(result)
+
+print("Chatbot session started. If you want to stop the chat, type exit")
+print(20 * "-")
+while True:
+    message = input("You: ")
+    if message.lower() == "exit":
+        print("Stopping the chat... bye.")
+        break
+
+    response = send_message(message, history)
+    if response:
+        bot_response = response.get("response", "")
+        history.extend([f"User: {message}", f"Bot: {bot_response}"])
+        print(f"Bot: {bot_response}")
